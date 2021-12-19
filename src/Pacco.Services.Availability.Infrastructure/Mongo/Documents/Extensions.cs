882a4de0df327c9a6b1ebc64017c85678bf80402
oleh.krutii <oleh.krutii@justanswer.com>
2021-12-19 16:58:58 +0200

    implement infrastructure

---
+++ src/Pacco.Services.Availability.Infrastructure/Mongo/Documents/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using Pacco.Services.Availability.Core.Entities;
+using Pacco.Services.Availability.Core.ValueObjects;
+using System;
+using System.Linq;
+
+namespace Pacco.Services.Availability.Infrastructure.Mongo.Documents
+{
+    internal static class Extensions
+    {
+        public static Resource AsEntity(this ResourceDocument document)
+            => new Resource(
+                document.Id,
+                document.Tags,
+                document.Reservations?.Select(r => new Reservation(r.TimeStamp.AsDateTime(), r.Priority)).ToList());
+
+        public static ResourceDocument AsDocument(this Resource entity)
+            => new ResourceDocument
+            {
+                Id = entity.Id,
+                Tags = entity.Tags,
+                Reservations = entity.Reservations?.Select(r => new ReservationDocument
+                {
+                    Priority = r.Priority,
+                    TimeStamp = r.DateTime.AsDaysSinceEpoch(),
+                }).ToList(),
+            };
+
+        public static int AsDaysSinceEpoch(this DateTime dateTime)
+            => (dateTime - new DateTime()).Days;
+
+        public static DateTime AsDateTime(this int daysSinceEpoch)
+            => new DateTime().AddDays(daysSinceEpoch);
+    }
+}
---
+++ src/Pacco.Services.Availability.Infrastructure/Mongo/Repositories/ResourcesMongoRepository.cs
@@ -0,0 +1,33 @@
+﻿using Convey.Persistence.MongoDB;
+using Pacco.Services.Availability.Core.Entities;
+using Pacco.Services.Availability.Core.Repositories;
+using Pacco.Services.Availability.Infrastructure.Mongo.Documents;
+using System;
+using System.Threading.Tasks;
+
+namespace Pacco.Services.Availability.Infrastructure.Mongo.Repositories
+{
+    internal sealed class ResourcesMongoRepository : IResourcesRepository
+    {
+        private readonly IMongoRepository<ResourceDocument, Guid> _repository;
+
+        public ResourcesMongoRepository(IMongoRepository<ResourceDocument, Guid> repository)
+            => _repository = repository;
+
+        public async Task<Resource> GetAsync(AggregateId id)
+        {
+            var document = await _repository.GetAsync(id);
+
+            return document?.AsEntity();
+        }
+
+        public Task AddAsync(Resource resource)
+            => _repository.AddAsync(resource.AsDocument());
+
+        public Task UpdateAsync(Resource resource)
+            => _repository.UpdateAsync(resource.AsDocument(), r => r.Id == resource.Id && r.Version < resource.Version);
+
+        public Task DeleteAsync(AggregateId id)
+            => _repository.DeleteAsync(id);
+    }
+}
