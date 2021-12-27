108bd784d5d15fc48bd4f1b0589548f9f12b64cf
oleh.krutii <oleh.krutii@justanswer.com>
2021-12-27 16:53:49 +0200

    implement outbox-inbox patterns

--- src/Pacco.Services.Availability.Infrastructure/Services/MessageBroker.cs
+++ src/Pacco.Services.Availability.Infrastructure/Services/MessageBroker.cs
@@ -1,6 +1,7 @@
 ﻿using Convey;
 using Convey.CQRS.Events;
 using Convey.MessageBrokers;
+using Convey.MessageBrokers.Outbox;
 using Microsoft.Extensions.Logging;
 using Pacco.Services.Availability.Application.Services;
 using System;
@@ -13,11 +14,13 @@ namespace Pacco.Services.Availability.Infrastructure.Services
     public class MessageBroker : IMessageBroker
     {
         private readonly IBusPublisher _busPublisher;
+        private readonly IMessageOutbox _messageOutbox;
         private readonly ILogger<MessageBroker> _logger;
 
-        public MessageBroker(IBusPublisher busPublisher, ILogger<MessageBroker> logger)
+        public MessageBroker(IBusPublisher busPublisher, IMessageOutbox messageOutbox, ILogger<MessageBroker> logger)
         {
             _busPublisher = busPublisher;
+            _messageOutbox = messageOutbox;
             _logger = logger;
         }
 
@@ -39,6 +42,12 @@ namespace Pacco.Services.Availability.Infrastructure.Services
 
                 var messageId = Guid.NewGuid().ToString("N");
                 _logger.LogTrace($"Publishing an integration event: '{@event.GetType().Name.Underscore()}' with ID '{messageId}'");
+
+                if (_messageOutbox.Enabled)
+                {
+                    await _messageOutbox.SendAsync(@event, messageId: messageId);
+                    continue;
+                }
 
                 await _busPublisher.PublishAsync(@event, messageId);
             }
