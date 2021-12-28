615926fdb18885dbff34be204bdb12b7e4ceec03
oleh.krutii <oleh.krutii@justanswer.com>
2021-12-28 17:24:39 +0200

    pass correlation id

--- src/Pacco.Services.Availability.Infrastructure/Services/MessageBroker.cs
+++ src/Pacco.Services.Availability.Infrastructure/Services/MessageBroker.cs
@@ -15,12 +15,21 @@ namespace Pacco.Services.Availability.Infrastructure.Services
     {
         private readonly IBusPublisher _busPublisher;
         private readonly IMessageOutbox _messageOutbox;
+        private readonly IMessagePropertiesAccessor _messagePropertiesAccessor;
+        private readonly ICorrelationContextAccessor _correlationContextAccessor;
         private readonly ILogger<MessageBroker> _logger;
 
-        public MessageBroker(IBusPublisher busPublisher, IMessageOutbox messageOutbox, ILogger<MessageBroker> logger)
+        public MessageBroker(
+            IBusPublisher busPublisher,
+            IMessageOutbox messageOutbox,
+            IMessagePropertiesAccessor messagePropertiesAccessor,
+            ICorrelationContextAccessor correlationContextAccessor,
+            ILogger<MessageBroker> logger)
         {
             _busPublisher = busPublisher;
             _messageOutbox = messageOutbox;
+            _messagePropertiesAccessor = messagePropertiesAccessor;
+            _correlationContextAccessor = correlationContextAccessor;
             _logger = logger;
         }
 
@@ -32,6 +41,9 @@ namespace Pacco.Services.Availability.Infrastructure.Services
             {
                 return;
             }
+
+            var correlationId = _messagePropertiesAccessor.MessageProperties?.CorrelationId;
+            var correlationContext = _correlationContextAccessor.CorrelationContext;
 
             foreach (var @event in events)
             {
@@ -45,11 +57,11 @@ namespace Pacco.Services.Availability.Infrastructure.Services
 
                 if (_messageOutbox.Enabled)
                 {
-                    await _messageOutbox.SendAsync(@event, messageId: messageId);
+                    await _messageOutbox.SendAsync(@event, messageId: messageId, correlationId: correlationId, messageContext: correlationContext);
                     continue;
                 }
 
-                await _busPublisher.PublishAsync(@event, messageId);
+                await _busPublisher.PublishAsync(@event, messageId, correlationId: correlationId, messageContext: correlationContext);
             }
         }
     }
